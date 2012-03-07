0f87b4e82da739321c304fa2861242b97547ad4b
Davor Babic <davor@davor.se>
2012-03-07 09:19:18 +0100

    Moved test files to test/ folder and wrote test_computers test for ConfigReader

---
+++ test/TestConfigReader.rb
@@ -0,0 +1,24 @@
+require "../ConfigReader.rb"
+require "test/unit"
+
+class TestConfigReader < Test::Unit::TestCase
+  def setup
+    @c = ConfigReader.new "testcomputers.yaml"
+  end
+
+  def test_file_not_found
+    assert_raise (Errno::ENOENT) { ConfigReader.new("i_dont_exist") }
+  end
+
+  def test_rooms
+    assert_equal Rooms, @c.rooms
+  end
+
+  def test_computers
+    assert_equal Computers, @c.computers
+  end
+end
+
+Rooms = ["dog", "cat", "house"]
+Computers = ["dog-1", "cat-1", "cat-2", "cat-3",
+               "house-0", "house-1"]
