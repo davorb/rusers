2e9a81be2e393f983f6143c3fd7fa8f180d55e95
Davor Babic <davor@davor.se>
2012-03-07 10:03:42 +0100

    Wrote Connection and TestConnection. Can connect to computers and fetch data now

---
+++ Connection.rb
@@ -0,0 +1,34 @@
+class Connection
+  attr_reader :host_name, :user_name, :command, :tunnel, :ssh_command
+
+  def initialize (host_name, command="w", user_name="dt08db2", 
+                  tunnel="login.student.lth.se")
+    @host_name = host_name
+    @user_name = user_name
+    @command = command
+    @tunnel = tunnel
+
+    set_ssh_command
+  end
+
+  def to_s
+    tmp = IO.popen @ssh_command
+    tmp.read
+  end
+
+  def command= (c)
+    @command = c
+    set_ssh_command
+  end
+
+  def user_name= (u)
+    @user_name = u
+    set_ssh_command
+  end
+
+  private
+  def set_ssh_command
+    @ssh_command = "ssh #{@user_name}@#{@tunnel} ssh"+
+      " -o StrictHostKeyChecking=no -q #{@host_name} #{@command}"
+  end
+end
---
+++ test/TestConnection.rb
@@ -0,0 +1,44 @@
+require "../Connection.rb"
+require "test/unit.rb"
+
+class TestConnection < Test::Unit::TestCase
+  def setup
+    @c = Connection.new "mars-1"
+  end
+
+  def test_read_default_values
+    assert_equal "mars-1", @c.host_name
+    assert_equal "dt08db2", @c.user_name
+    assert_equal "login.student.lth.se", @c.tunnel
+    assert_equal "w", @c.command
+  end
+
+  def test_connect_to_server
+    lines = Array.new
+    conn = Connection.new "login.student.lth.se"
+    result = conn.to_s
+    result.each_line { |l| lines << l }
+    assert_equal Second_line, lines[1]
+  end
+
+  def test_set_methods
+    @c.command = "LOL"
+    @c.user_name = "idontexist"
+
+    assert_equal "idontexist", @c.user_name
+    assert_equal "LOL", @c.command
+
+    expected_command =
+      'ssh idontexist@login.student.lth.se ssh'\
+      ' -o StrictHostKeyChecking=no -q mars-1 LOL'
+    assert_equal expected_command, @c.ssh_command
+  end
+
+  def test_connect_to_nowhere
+    conn = Connection.new "idontexist"
+    assert_equal "", conn.to_s
+  end
+end
+
+Second_line = 'USER     TTY      FROM              LOGIN@   IDLE   JCPU   PCPU WHAT
+'
